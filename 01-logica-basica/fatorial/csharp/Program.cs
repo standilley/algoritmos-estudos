a9e7e86415b171982af97ed914c3f289ff0e78a8
stand.personal <stand@DESKTOP-1PVOLSM>
2025-12-04 20:51:54 -0300

    feat: adicionar exercício de fatorial

---
+++ 01-logica-basica/fatorial/csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int fatorial = 1;
+
+for (int i = 1; i <= numero; i++)
+{
+    fatorial = fatorial * i;
+}
+
+Console.WriteLine($"Fatorial de {numero} = {fatorial}");

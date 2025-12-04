174e23e95fa031edb70573f7aa190ad54275f997
stand.personal <stand@DESKTOP-1PVOLSM>
2025-12-04 20:28:50 -0300

    chore: sincronizar develop local com a remota

--- 01-logica-basica/menor-numero/csharp/obj/Debug/net10.0/csharp.AssemblyInfo.cs
+++ 01-logica-basica/menor-numero/csharp/obj/Debug/net10.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc201045e13ebd9bb73698cdc6d27d5d2a5a635c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24027382db3dad3fbe5a05aae685d7cf875711e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

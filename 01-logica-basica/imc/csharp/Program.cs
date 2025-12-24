c55357119c0094237c9edb57bdfd3c4602f5b0cf
stand.personal <stand@DESKTOP-1PVOLSM>
2025-12-24 11:36:21 -0300

    feat: adicionar calculo imc

---
+++ 01-logica-basica/imc/csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite o peso (kg): ");
+double peso = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura (m): ");
+double altura = double.Parse(Console.ReadLine());
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"IMC: {imc:F2}");
+
+if (imc < 18.5)
+    Console.WriteLine("Classificação: Abaixo do peso");
+else if (imc < 25)
+    Console.WriteLine("Classificação: Peso normal");
+else if (imc < 30)
+    Console.WriteLine("Classificação: Sobrepeso");
+else
+    Console.WriteLine("Classificação: Obesidade");
